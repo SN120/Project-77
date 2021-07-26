e0e65db3629cba86d43bacd76452e5d6df98c346
SN120 <79451292+SN120@users.noreply.github.com>
2021-07-26 18:29:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("My first console project");
+
+var firstname = "Siddhant";
+var lastname = "Narayanan";
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+
+var number1 = 92;
+var number2 = 49;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2)
